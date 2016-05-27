0df4b7ad005064625e36a41ba6c8c490157530ba
Pascal Bigot <pascal.bigot.ms@gmail.com>
2016-05-27 11:36:45 -0400

    Updated vsvars32.bat to be able to use various Visual studio version

--- vsvars32.bat
+++ vsvars32.bat
@@ -1,12 +1,47 @@
-@SET VSINSTALLDIR=e:\Program Files\Microsoft Visual Studio 9.0
-@SET VCINSTALLDIR=e:\Program Files\Microsoft Visual Studio 9.0\VC
+@SET VS_DRIVE=C
+
+@IF EXIST "%VS_DRIVE%:\Program Files (x86)\Microsoft Visual Studio 10.0\VC" GOTO SETUP_VC10_X64
+@IF EXIST "%VS_DRIVE%:\Program Files\Microsoft Visual Studio 10.0\VC" GOTO SETUP_VC10_X86
+@IF EXIST "%VS_DRIVE%:\Program Files (x86)\Microsoft Visual Studio 9.0\VC" GOTO SETUP_VC9_X64
+@IF EXIST "%VS_DRIVE%:\Program Files\Microsoft Visual Studio 9.0\VC" GOTO SETUP_VC9_X86
+
+@echo "No Visual Studio setup found"
+exit
+
+:SETUP_VC9_X86
+@echo "SETUP_VC9_X86"
+@SET VSINSTALLDIR=%VS_DRIVE%:\Program Files\Microsoft Visual Studio 9.0
+@SET VCINSTALLDIR=%VS_DRIVE%:\Program Files\Microsoft Visual Studio 9.0\VC
+@GOTO CONTINUE
+
+:SETUP_VC9_X64
+@echo "SETUP_VC9_X64"
+@SET VSINSTALLDIR=%VS_DRIVE%:\Program Files (x86)\Microsoft Visual Studio 9.0
+@SET VCINSTALLDIR=%VS_DRIVE%:\Program Files (x86)\Microsoft Visual Studio 9.0\VC
+@GOTO CONTINUE
+
+:SETUP_VC10_X86
+@echo "SETUP_VC10_X86"
+@SET VSINSTALLDIR=%VS_DRIVE%:\Program Files\Microsoft Visual Studio 10.0
+@SET VCINSTALLDIR=%VS_DRIVE%:\Program Files\Microsoft Visual Studio 10.0\VC
+@GOTO CONTINUE
+
+:SETUP_VC10_X64
+@echo "SETUP_VC10_X64"
+@SET VSINSTALLDIR=%VS_DRIVE%:\Program Files (x86)\Microsoft Visual Studio 10.0
+@SET VCINSTALLDIR=%VS_DRIVE%:\Program Files (x86)\Microsoft Visual Studio 10.0\VC
+@GOTO CONTINUE
+
+:CONTINUE
+
+@ECHO VSINSTALLDIR=%VSINSTALLDIR%
+@ECHO VCINSTALLDIR=%VCINSTALLDIR%
 @SET FrameworkDir=C:\Windows\Microsoft.NET\Framework
 @SET FrameworkVersion=v2.0.50727
 @SET Framework35Version=v3.5
 @if "%VSINSTALLDIR%"=="" goto error_no_VSINSTALLDIR
 @if "%VCINSTALLDIR%"=="" goto error_no_VCINSTALLDIR
 
-@echo Setting environment for using Microsoft Visual Studio 2008 x86 tools.
 
 @call :GetWindowsSdkDir
 
@@ -20,12 +55,12 @@
 @rem
 @rem Root of Visual Studio IDE installed files.
 @rem
-@set DevEnvDir=e:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE
+@set DevEnvDir=%VSINSTALLDIR%\Common7\IDE
 
-@set PATH=e:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE;e:\Program Files\Microsoft Visual Studio 9.0\VC\BIN;e:\Program Files\Microsoft Visual Studio 9.0\Common7\Tools;C:\Windows\Microsoft.NET\Framework\v3.5;C:\Windows\Microsoft.NET\Framework\v2.0.50727;e:\Program Files\Microsoft Visual Studio 9.0\VC\VCPackages;%PATH%
-@set INCLUDE=e:\Program Files\Microsoft Visual Studio 9.0\VC\INCLUDE;%INCLUDE%
-@set LIB=e:\Program Files\Microsoft Visual Studio 9.0\VC\LIB;%LIB%
-@set LIBPATH=C:\Windows\Microsoft.NET\Framework\v3.5;C:\Windows\Microsoft.NET\Framework\v2.0.50727;e:\Program Files\Microsoft Visual Studio 9.0\VC\LIB;%LIBPATH%
+@set PATH=%VSINSTALLDIR%\Common7\IDE;%VSINSTALLDIR%\VC\BIN;%VSINSTALLDIR%\Common7\Tools;C:\Windows\Microsoft.NET\Framework\v3.5;C:\Windows\Microsoft.NET\Framework\v2.0.50727;%VSINSTALLDIR%\VC\VCPackages;%PATH%
+@set INCLUDE=%VCINSTALLDIR%\INCLUDE;%INCLUDE%
+@set LIB=%VCINSTALLDIR%\LIB;%LIB%
+@set LIBPATH=C:\Windows\Microsoft.NET\Framework\v3.5;C:\Windows\Microsoft.NET\Framework\v2.0.50727;%VSINSTALLDIR%\VC\LIB;%LIBPATH%
 
 @goto end
 
